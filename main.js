796554a3906c0fd83055b8650f96214cfb901fb7
Michael Law <michaellaw@gmx.com>
2022-03-14 14:34:36 -0400

    Added JS event

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 alert('JavaScript is running!')
+
+let btn = document.getElementById('btn')
+// noSpacesInTheWord with a captial letter every word is camel case
+btn.addEventListener('click', () => {
+  alert('Clicked!')
+})
